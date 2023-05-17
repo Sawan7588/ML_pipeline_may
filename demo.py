c534b030bddaccbc5f82162103cc70692db06ba0
mongodbtest <palasiyasawan756@gmail.com>
2023-05-17 23:50:12 +0530

    Exception completed

--- demo.py
+++ demo.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from visa.logger import logging
+from visa.exception import CustomException
+import os
+import sys
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    logging.info("we are testing our logging file")
-    return "Hello worlds"
+    try:
+        raise Exception (" we are testing our custom exception file")
+    except Exception as e:
+        visa = CustomException(e,sys)
+        logging.info(visa.error_message)
+        logging.info("we are testing logging module")
+        return "hello World"
+    
+    
 
 
 if __name__=="__main__":
