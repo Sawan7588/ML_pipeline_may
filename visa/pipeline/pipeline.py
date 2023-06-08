8f5dd26b389167e00b84d0fa0e2d9b732c41d668
mongodbtest <palasiyasawan756@gmail.com>
2023-06-08 19:47:22 +0530

    data ingestion checking

--- visa/pipeline/pipeline.py
+++ visa/pipeline/pipeline.py
@@ -3,16 +3,16 @@ import pandas as pd
 import numpy as np
 from visa.constant import *
 from visa.logger import logging
-from visa.entity.config_entity import DataIngestionConfig
 from visa.entity.artifact_entity import DataIngestionArtifact
 from visa.exception import CustomException
 from datetime import date
 from collections import namedtuple
-from visa.config.configuration import Configuration
+from visa.config.configuration import Configuartion
 from visa.components.data_ingestion import DataIngestion
 
+
 class Pipeline():
-    def __init__(self, config: Configuration = Configuration())->None:
+    def __init__(self, config: Configuartion = Configuartion())->None:
         try:
             self.config = config
         except Exception as e:
@@ -21,15 +21,17 @@ class Pipeline():
     def start_data_ingestion(self)-> DataIngestionArtifact:
         try:
             data_ingestion = DataIngestion(data_ingestion_config= self.config.get_data_ingestion_config())
-            return data_ingestion.initiate_data_ingestion
+            return data_ingestion.initiate_data_ingestion()
         except Exception as e:
             raise CustomException(e,sys) from e  
-        
-    
         
     def run_pipeline(self):
         try:
             # Data Ingestion
             data_ingestion_artifact = self.start_data_ingestion()
+            
         except Exception as e:
             raise CustomException(e,sys) from e  
+        
+
+    
